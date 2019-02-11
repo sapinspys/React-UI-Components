b00f5c1242110c72a0f8bade4ebafd6e56d956d2
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-11 18:39:27 -0500

    Integrated footer icons (STRETCH)

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -4,7 +4,12 @@ import './Footer.css';
 function Footer() {
     return (
         <footer>
-            
+            <i class="far fa-comments"></i>
+            <i class="fas fa-sync-alt"></i>
+            <i class="far fa-heart"></i>
+            <i class="far fa-envelope"></i>
         </footer>
     );
 };
+
+export default Footer;
