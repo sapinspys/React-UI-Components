4b2f5434395c55fffa227587446284b7611fb0e2
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-11 17:13:50 -0500

    Created barebones CardContainer component

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Card.css';
+
+function CardContainer() {
+    return (
+        <section className="card">
+            <CardBanner />
+            <CardContent />
+        </section>
+    );
+}
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 function HeaderTitle() {
     return (
-        <p><strong>Lambda School</strong><span className="handleTime"> @LambdaSchool &#183; 26 Jan</span></p>
+        <p><strong>Lambda School</strong><span className="handleTime"> @LambdaSchool &middot; 26 Jan</span></p>
     );
 };
 
