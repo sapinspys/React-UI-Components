b76f85fbbe600e9ea9c342dbfd642f958e2ab6de
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-11 15:48:57 -0500

    Built header container component, working on importing its inner components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from "./ImageThumbnail";
+
+
+function HeaderContainer() {
+    return (
+        <div className='header-container'>
+            <ImageThumbnail />
+            <HeaderTitle />
+            <HeaderContent />
+        </div>
+    );
+};
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+function ImageThumbnail() {
+    return <img src="./img/lambdacrest.png" alt="lambda-crest" />
+};
+
+export default ImageThumbnail;
