1d1c140e5e9b2daedf5585212da8abafdbb3fe17
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-11 18:12:26 -0500

    Added onClick event listener! MVP NOW COMPLETED... SORRY :)

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -3,10 +3,13 @@ import './Card.css';
 import CardBanner from "./CardBanner";
 import CardContent from "./CardContent";
 
+function toReact() {
+    window.open(`https://www.reactjs.org`);
+};
 
 function CardContainer() {
     return (
-        <section className="card">
+        <section className="card" onClick={toReact}>
             <CardBanner />
             <CardContent />
         </section>
