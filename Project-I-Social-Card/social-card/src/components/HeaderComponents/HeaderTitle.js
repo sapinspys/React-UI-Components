cc30ed827ad6808a5d0203ded3d7f2f442a2559f
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-11 15:55:25 -0500

    Built inner components of header container, now to import

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    return (
+        <p>Lambda School<span className="handleTime"> @LambdaSchool &middot 26 Jan</span></p>
+    );
+};
