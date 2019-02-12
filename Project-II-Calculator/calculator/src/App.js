bf5f8a6e86b03895d54ee7ec8fcc33d16432fe6b
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-12 14:13:20 -0500

    Re-factored app.js to be a classical component and added state (STRETCH)

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -4,43 +4,35 @@ import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 import ActionButton from './components/ButtonComponents/ActionButton';
 import NumberButton from './components/ButtonComponents/NumberButton';
 
-const App = () => {
-  return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {operations:[]}
+  }
 
+  render() {
+    return (
       <div className='calculator'>
-        <CalculatorDisplay text='0'/>
-        <ActionButton text='clear'/>
-        <NumberButton text='/' buttonStyle='function-btn'/>
-        <NumberButton text='7' buttonStyle='number-btn'/>
-        <NumberButton text='8' buttonStyle='number-btn'/>
-        <NumberButton text='9' buttonStyle='number-btn'/>
-        <NumberButton text='x' buttonStyle='function-btn'/>
-        <NumberButton text='4' buttonStyle='number-btn'/>
-        <NumberButton text='5' buttonStyle='number-btn'/>
-        <NumberButton text='6' buttonStyle='number-btn'/>
-        <NumberButton text='-' buttonStyle='function-btn'/>
-        <NumberButton text='1' buttonStyle='number-btn'/>
-        <NumberButton text='2' buttonStyle='number-btn'/>
-        <NumberButton text='3' buttonStyle='number-btn'/>
-        <NumberButton text='+' buttonStyle='function-btn'/>
-        <ActionButton text='0'/>
-        <NumberButton text='=' buttonStyle='function-btn'/>
+        <CalculatorDisplay data={this.state.operations}/>
+        <ActionButton onClick={this.handleClick} text='clear'/>
+        <NumberButton onClick={this.handleClick} text='/' buttonStyle='function-btn'/>
+        <NumberButton onClick={this.handleClick} text='7' buttonStyle='number-btn'/>
+        <NumberButton onClick={this.handleClick} text='8' buttonStyle='number-btn'/>
+        <NumberButton onClick={this.handleClick} text='9' buttonStyle='number-btn'/>
+        <NumberButton onClick={this.handleClick} text='x' buttonStyle='function-btn'/>
+        <NumberButton onClick={this.handleClick} text='4' buttonStyle='number-btn'/>
+        <NumberButton onClick={this.handleClick} text='5' buttonStyle='number-btn'/>
+        <NumberButton onClick={this.handleClick} text='6' buttonStyle='number-btn'/>
+        <NumberButton onClick={this.handleClick} text='-' buttonStyle='function-btn'/>
+        <NumberButton onClick={this.handleClick} text='1' buttonStyle='number-btn'/>
+        <NumberButton onClick={this.handleClick} text='2' buttonStyle='number-btn'/>
+        <NumberButton onClick={this.handleClick} text='3' buttonStyle='number-btn'/>
+        <NumberButton onClick={this.handleClick} text='+' buttonStyle='function-btn'/>
+        <ActionButton onClick={this.handleClick} text='0'/>
+        <NumberButton onClick={this.handleClick} text='=' buttonStyle='function-btn'/>
       </div>
-    </div>
-  );
+    )
+  };
 };
 
 export default App;
