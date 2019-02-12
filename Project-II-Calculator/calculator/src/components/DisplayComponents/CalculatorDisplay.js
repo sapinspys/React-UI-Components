63ee805eb03612132fe16a9b4e318de46e881a0e
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-12 14:26:14 -0500

    Re-factored CalculatorDisplay to a classical component

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './Display.css';
 
-const CalculatorDisplay = props => {
-    return <div className='calc-display'>{props.text}</div>
+// const CalculatorDisplay = props => {
+//     return <div className='calc-display'>{props.text}</div>
+// }
+
+class CalculatorDisplay extends React.Component {
+    render () {
+        const string = this.props.data.join('');
+        return <div 
+            className='calc-display'>
+            {string}
+        </div>
+    }
 }
 
 export default CalculatorDisplay;
