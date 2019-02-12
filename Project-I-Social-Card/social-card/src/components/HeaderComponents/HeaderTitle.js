653f0a70fd555d9651f7a8e1bc2304d5647f2c5b
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-11 19:43:21 -0500

    Integrated moment.js (STRETCH)

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Header.css';
+import moment from "moment";
 
 function HeaderTitle() {
     return (
-        <p><strong>Lambda School</strong><span className="soft"> @LambdaSchool &middot; 26 Jan</span></p>
+        <p><strong>Lambda School</strong><span className="soft"> @LambdaSchool &middot; {moment().format("MMM Do")}  </span></p>
     );
 };
 
