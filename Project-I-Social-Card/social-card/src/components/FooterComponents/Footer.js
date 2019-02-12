62bc43fae3e4502d87d2fb579b66e9205def85d8
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-11 20:03:17 -0500

    Manually zoomed in React banner using external crop

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -4,10 +4,10 @@ import './Footer.css';
 function Footer() {
     return (
         <footer>
-            <i class="far fa-comments"></i>
-            <i class="fas fa-sync-alt"></i>
-            <i class="far fa-heart"></i>
-            <i class="far fa-envelope"></i>
+            <i className="far fa-comments"></i>
+            <i className="fas fa-sync-alt"></i>
+            <i className="far fa-heart"></i>
+            <i className="far fa-envelope"></i>
         </footer>
     );
 };
